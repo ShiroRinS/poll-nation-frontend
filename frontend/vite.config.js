26c44bc7855eb6681c47538fe0b09bd81a5a8dc8
Rin <ashiyashakira@gmail.com>
2024-12-01 21:28:00 +0700

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -13,7 +13,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: 'http://3.95.212.66:3000',
         changeOrigin: true,
         secure: false
 
